77e06971a8b8d1f8c933c1539d3a953a91195ead
Shea Bunge <bungeshea@gmail.com>
2017-08-24 08:51:30 +1000

    Fill in assignment two

--- kit103_assign2.py
+++ kit103_assign2.py
@@ -1,4 +1,4 @@
-'''
+"""
 KIT103/KMA155 Programming Assignment 2: Logic
 Submission script
 
@@ -8,75 +8,88 @@ ID: Replace with your student ID
 Enter your answers to each question below by completing each function
 or, in the case of Question 4a, filling in the Karnaugh map.
 After answering a question run this script and test your implementation.
-'''
+"""
 
 # Question 1: Riding the Sine Wave
 
 REJECT, FIND_ADULT, ACCEPT = 'Sorry, you cannot ride', 'Find an adult', 'You can ride'
 
+
 def q1_sine_wave_check(height, age, has_adult):
-    '''Returns one of three string messages depending on whether a
-    person can ride the Sine Wave or not.
-    Parameters:
-    height - integer height in cm
-    age - integer age in years
-    has_adult - True iff the person is accompanied by an adult
-    '''
-    return REJECT
+	"""Returns one of three string messages depending on whether a
+	person can ride the Sine Wave or not.
+	Parameters:
+	height - integer height in cm
+	age - integer age in years
+	has_adult - True iff the person is accompanied by an adult
+	"""
+	if 110 > height or height > 200 or age < 6:
+		return REJECT
+
+	if 6 <= age <= 9 and not has_adult:
+		return FIND_ADULT
+
+	return ACCEPT
 
 
 # Question 2: Implementing predicates as functions
 
 def q2_a(a, b):
-    return None
+	# ¬(a ^ b) ^ (a v b)
+	return not (a and b) and (a or b)
+
 
 def q2_b(a, b, c, d):
-    return None
+	# a v (¬b v ¬c v ¬d)
+	return a or (not b or not c or not d)
+
 
 def q2_c(a, b, c):
-    return None
+	# (a v b) ^ (a v c)
+	return (a or b) and (a or c)
+
 
 def q2_d(a):
-    return None
+	# a ^ ¬a
+	return a and not a
 
 
 # Question 3: Simplifying predicates
 
 def q3_a(a, b):
-    return None
+	# a xor b
+	return a ^ b
+
 
 def q3_b(a, b, c, d):
-    return None
+	# a v ¬(b ^ c ^ d)
+	return a or not (b and c and d)
+
 
 def q3_c(a, b, c):
-    return None
+	# a v b ^ c
+	return a or b and c
+
 
 def q3_d(a):
-    return None
+	return False
 
 
 # Question 4: Simplifying a predicate using a Karnaugh map
 
-#Part a: Letter Detector Karnaugh Map --- replace the appropriate 0 entries with 1 (representing True)
+# Part a: Letter Detector Karnaugh Map --- replace the appropriate 0 entries with 1 (representing True)
 q4_kmap = [
-#cd\ab
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
+	# cd\ab
+	[0, 1, 0, 0],
+	[1, 0, 0, 0],
+	[1, 1, 1, 1],
+	[0, 0, 0, 0]
 ]
 
-#Part b: The detector function
-def q4_acme_letter_detector(a, b, c, d):
-    '''Returns True iff the curves present resemble a letter, False otherwise.'''
-    #Your task is to replace this with an equivalent but simpler expression.
-    # If you wish to split your expression over multiple lines then use a
-    # slash \ at the end of each line, as below.
-    return (not a and not b and not c and d) or \
-            (not a and not b and c and d) or \
-            (not a and b and not c and not d) or \
-            (not a and b and c and d) or \
-            (a and not b and c and d) or \
-            (a and b and c and d)
 
-#End of answers
+# Part b: The detector function
+def q4_acme_letter_detector(a, b, c, d):
+	"""Returns True iff the curves present resemble a letter, False otherwise."""
+	return (c and d) or (not a and not b and d) or (not a and b and not c and not d)
+
+# End of answers
