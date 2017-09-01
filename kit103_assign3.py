c4f9dfebae584121c85594cd11626a900109ec27
Shea Bunge <bungeshea@gmail.com>
2017-09-01 22:20:40 +1000

    Add provided files for assignment three

---
+++ kit103_assign3.py
@@ -0,0 +1,82 @@
+'''
+KIT103/KMA155 Programming Assignment 3: Number Theory part 1
+Submission script
+
+Name: Replace with your name
+ID: Replace with your student ID
+
+Enter your answers to each question below by completing each
+function. After answering a question run this script and test
+your implementation in the IPython console.
+'''
+
+
+# Question 1: Divisibility of really, really big integers (2 marks)
+
+def divisible_by_2(s):
+    '''Returns True if the number represented by the string s is
+    divisible by 2, False otherwise.'''
+    return None
+
+def divisible_by_3(s):
+    '''Returns True if the number represented by the string s is
+    divisible by 3, False otherwise.'''
+    return None
+
+def divisible_by_4(s):
+    '''Returns True if the number represented by the string s is
+    divisible by 4, False otherwise.'''
+    return None
+
+def divisible_by_11(s):
+    '''Returns True if the number represented by the string s is
+    divisible by 11, False otherwise.'''
+    return None
+
+
+# Question 2: GCD from a prime factorisations (2 marks)
+
+from collections import Counter
+
+def q2_factor_gcd(a, b):
+    '''Returns gcd(a, b), calculated from their prime factorisations.'''
+    return None
+
+
+# Question 3: Are a and b coprime (i.e., relatively prime)? (1 mark)
+
+# Implement your additional helper function here
+
+def q3_coprime(a, b):
+    '''Returns True if a and b are coprime, False otherwise.'''
+    return None
+
+
+# End of answers
+
+
+# Provided functions
+
+from math import floor, sqrt
+
+def primes(n):
+    primes = set(range(2, n+1))
+    for k in range(2, floor(sqrt(n))+1):
+        if k in primes:
+            primes.difference_update( range(k**2, n+1, k) )
+    return primes
+
+def primes_list(n):
+    return sorted(primes(n))
+
+def factor_list(n):
+    factors = []
+    iprimes = iter( primes_list(n) )
+    while n > 1:
+        p = next(iprimes)
+        while n % p == 0:
+            n = n // p
+            factors.append(p)
+    return factors
+
+# End of provided functions
