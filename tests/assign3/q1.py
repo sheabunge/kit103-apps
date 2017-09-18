f48555e119f90cd39dbf1e4277abd5a093472d82
Shea Bunge <bungeshea@gmail.com>
2017-09-18 15:10:39 +1000

    Fix broken q1 assignment 3 test

--- tests/assign3/q1.py
+++ tests/assign3/q1.py
@@ -1,18 +1,18 @@
 from random import randrange
 import kit103_assign3 as assign3
 
-PRINT_PASSED = False
+PRINT_PASSED = True
 
 
-def test(d, cases=20, limit=10):
+def test(d, cases=100, limit=10e5):
 	func = getattr(assign3, f'divisible_by_{d}')
 
 	for _ in range(cases):
-		n = randrange(limit) + 2
+		n = randrange(limit)
 		divides = n % d == 0
 
 		if func(str(n)) != divides:
-			print(f'Failure: {n} should {"not " if divides else ""}be divisible by {d}')
+			print(f'Failure: {n} should {"" if divides else "not "}be divisible by {d}')
 		elif PRINT_PASSED:
 			print(f'Success: {d} {" |" if divides else "!|"} {n}')
 
